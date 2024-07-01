8a1e63b67833af5e7aac1e101b05f09af8dc3e24
keremsakarya <sakaryakerem252@gmail.com>
2024-07-01 18:26:47 +0300

    first commit

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Footer.css"
+
+const Footer = () => {
+    return (
+        <div className='footer'>
+            <p>Copyright © 2024, CoinzzField - All Rights Reserved.</p>
+        </div>
+    )
+}
+
+export default Footer
